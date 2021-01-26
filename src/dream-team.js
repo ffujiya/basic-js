81813901f9c9fdafde83ebe9e73b5b5aca60ef1f
fujiya <frolov604@gmail.com>
2021-01-26 23:44:17 +0300

    feat: implement 'count cats' and dream team tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  return !Array.isArray(members) ? false : members.
+    filter(item => typeof item === 'string').
+    map(el => el.trim().toUpperCase().charAt(0)).
+    sort().
+    join('');
 };
