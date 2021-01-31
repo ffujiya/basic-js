a1848590539516d16c6bdbe51e80b17e14298c5a
fujiya <frolov604@gmail.com>
2021-01-31 21:01:22 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(!sampleActivity 
+    || typeof sampleActivity !== 'string' 
+    || Number.parseFloat(sampleActivity) <= 0
+    || Number.parseFloat(sampleActivity) > MODERN_ACTIVITY
+    || !isFinite(Number.parseFloat(sampleActivity)))  return false;
+
+  let k=0.693/HALF_LIFE_PERIOD;
+  let age = Math.log(MODERN_ACTIVITY/Number.parseFloat(sampleActivity))/k;
+  return Math.ceil(age);
 };
