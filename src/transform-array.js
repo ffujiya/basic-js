beecf7f07e4ccfb3cc3a6c9d82094e310a4ddf78
fujiya <frolov604@gmail.com>
2021-02-14 20:32:17 +0300

    feat: implement transform-array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Argument is not an array!');
+  let initial = arr.slice();
+
+  for (let i = 0; i < initial.length; i++) {
+    switch (initial[i]) {
+      case '--discard-next':
+        if (initial[i + 1] !== undefined) {
+          initial.splice(i + 1, 1);
+        }
+        break;
+      case '--discard-prev':
+        if (initial[i - 1] !== undefined) {
+          initial.splice(i - 1, 1);
+        }
+        break;
+      case '--double-next':
+        if (initial[i + 1] !== undefined) {
+          initial.splice(i + 1, 0, initial[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (initial[i - 1] !== undefined) {
+          initial.splice(i - 1, 0, initial[i - 1]);
+          i++;
+        }
+        break;
+    }
+  }
+
+  return initial.filter(item => item !== '--discard-next' && item !== '--discard-prev' && item !== '--double-next' && item !== '--double-prev');
 };
