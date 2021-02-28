f60571539151a04b05448c33070e466cc7935557
fujiya <frolov604@gmail.com>
2021-02-28 18:45:14 +0300

    feat: implement extended-repeater task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
+  let addition = '';
+  let phrase = '';
+  let inputString = str!== undefined ? String(str) : '';
+  let additionString = 'addition' in options ? String(options.addition) : '';
+  let repeatTimes = options.repeatTimes!== undefined ? options.repeatTimes : 1;
+  let additionRepeatTimes = options.additionRepeatTimes!== undefined ? options.additionRepeatTimes : 1;
+  let separator = 'separator' in options ? String(options.separator) : '+';
+  let additionSeparator = 'additionSeparator' in options ? String(options.additionSeparator) : '|';
+
+  addition = `${additionString}${additionSeparator}`.repeat(additionRepeatTimes).slice(0, addition.length - additionSeparator.length);
+  phrase = `${inputString}${addition}${separator}`.repeat(repeatTimes).slice(0, phrase.length - separator.length);
   
+  return phrase;
+};
