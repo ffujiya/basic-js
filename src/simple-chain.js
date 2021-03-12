9729f1c12b6f827f3cb304a47150881931548fb3
fujiya <frolov604@gmail.com>
2021-03-12 22:53:50 +0300

    feat: implement simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(Number.isNaN(position) || position<0 || position>this.chain.length-1 || !Number.isInteger(position)) {
+      this.chain.length = 0;
+      throw new Error('wrong position');
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let s = '( ' + this.chain.map(x => String(x)).join(' )~~( ') + ' )';
+    this.chain.length = 0;
+    return s;
   }
 };
 
